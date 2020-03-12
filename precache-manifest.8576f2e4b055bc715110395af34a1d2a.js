98b61d21e134758a4be37e1cfd5c690bddf9ad83
Saravanan <sarvan.rb@gmail.com>
2020-03-12 17:05:18 +0530

    Updates

---
+++ precache-manifest.8576f2e4b055bc715110395af34a1d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2b8f5bf3cdb072b5c2e092ca92db5f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ee34f977d500502c0add",
+    "url": "/tic-tac-toe/static/css/main.3cf43e50.chunk.css"
+  },
+  {
+    "revision": "7eadc0a97e3ede9647cd",
+    "url": "/tic-tac-toe/static/js/2.d30f2bf6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tic-tac-toe/static/js/2.d30f2bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee34f977d500502c0add",
+    "url": "/tic-tac-toe/static/js/main.9cc9815b.chunk.js"
+  },
+  {
+    "revision": "e2033292bdf40307058b",
+    "url": "/tic-tac-toe/static/js/runtime-main.423d23ef.js"
+  }
+]);
